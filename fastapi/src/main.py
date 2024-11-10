1ea31fe4dae04d917cc084165fbf8fccd4b7e5b3
mehmet.kose <mehmet003@hotmail.be>
2024-11-10 16:22:11 +0100

    add html email

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -98,43 +98,18 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/email/send")
-async def send_in_background(
-    background_tasks: BackgroundTasks,
-    email: EmailSchema
-) -> JSONResponse:
+@app.post("/email")
+async def send_with_template(email: EmailSchema) -> JSONResponse:
 
     message = MessageSchema(
-        subject="Fastapi mail module",
+        subject="ProcLogic daily summary",
         recipients=email.get("email"),
-        body="Simple background task",
-        subtype=MessageType.plain)
-
-    fm = FastMail(conf)
-
-    background_tasks.add_task(fm.send_message, message)
-
-    return JSONResponse(status_code=200, content={"message": "email has been sent"})
-
-
-@app.post("/email/send-file")
-async def send_file(
-    background_tasks: BackgroundTasks,
-    file: UploadFile = File(...),
-    email: EmailStr = Form(...)
-) -> JSONResponse:
-
-    message = MessageSchema(
-        subject="Fastapi mail module",
-        recipients=[email],
-        body="Simple background task",
+        template_body=email.get("body"),
         subtype=MessageType.html,
-        attachments=[file])
+    )
 
     fm = FastMail(conf)
-
-    background_tasks.add_task(fm.send_message, message)
-
+    await fm.send_message(message, template_name="email_template.html")
     return JSONResponse(status_code=200, content={"message": "email has been sent"})
 
 
